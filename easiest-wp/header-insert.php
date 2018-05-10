48a332957974482d86b295b46a2399fdf2193fe4
kamo(mac) <kamotsuyo@gmail.com>
2018-05-10 23:11:31 +0900

    変数名変更

--- easiest-wp/header-insert.php
+++ easiest-wp/header-insert.php
@@ -3,8 +3,8 @@
 <!-- viewport -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- javascript -->
-<?PHP $require_js = get_theme_file_uri() . '/js/require.js'; ?>
-<?PHP $main_js=get_theme_file_uri() . '/js/main.js'; ?>
-<script data-main="<?PHP echo $main_js ?>" src="<?PHP echo $require_js?>"></script>
+<?PHP $require_js_uri = get_theme_file_uri() . '/js/require.js'; ?>
+<?PHP $main_js_uri=get_theme_file_uri() . '/js/main.js'; ?>
+<script data-main="<?PHP echo $main_js_uri ?>" src="<?PHP echo $require_js_uri?>"></script>
 <!--スタイルシート-->
 <link rel="stylesheet" id="default-style" href="<?PHP echo(get_stylesheet_uri()); ?>" media="all">
