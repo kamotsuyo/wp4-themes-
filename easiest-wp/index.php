5b74f7c8afe9d69b78700e7ca7d2420c7b486d96
kamo(mac) <kamotsuyo@gmail.com>
2018-05-22 16:07:51 +0900

    wp_enquiue_scriptスクリプト読み込みブランチ修正の影響でmasterブランチの修正が発生

--- easiest-wp/index.php
+++ easiest-wp/index.php
@@ -1,10 +1,8 @@
 <!--headerテンプレートの読み込み-->
 <?PHP get_header(); ?>
-<h1>
-    master</h1>
-<h2>履歴</h2>
-<p>wp_enqueue_scriptのテストmasterにPull Request</p>
-<p>Pull RequestをGitHubのウェブサイトでMarge承認した</p>
+
+<h1>wp4.themes</h1>
+<h2>master</h2>
 <section>
     <h2>投稿記事</h2>
     <!--記事一覧表示-->
@@ -49,83 +47,9 @@
     <?php endif;?>
 </section>
 
-<<<<<<< HEAD
+
 <nav>
-    <!--
-=======
-<body <?php body_class()?>>
-
-    <header>
-        <!-- サイトタイトルとキャッチフレーズ -->
-        <!-- echo文でurlを出力するときにはesc_url()関数でエスケープを行う-->
-        <a href="<?php echo esc_url(home_url());?>">
-            <?php bloginfo("name");?>
-        </a>
-        <p>
-            <?php bloginfo("description");?>
-        </p>
-        <!--グローバルナビゲーションメニュー-->
-
-        <?php if(has_nav_menu('global')): ?>
-        <?php $global_menu_array = array('theme_location'=>'global','menu_id'=>'global-menu','container'=>'nav','container_class'=>'global-nav');?>
-        <?php wp_nav_menu($global_menu_array); endif; ?>
-    </header>
-    <h1>master</h1>
-    <h2>履歴</h2>
-    <p>wp_enqueue_scriptのテストをmasterにPull Requestした</p>
-    <section>
-        <h2>投稿記事</h2>
-        <!--記事一覧表示-->
-        <?php if(have_posts()):?>
-        <ul>
-            <?php while(have_posts()) :?>
-            <?php the_post();?>
-            <li>
-                <!--投稿日　echo get_the_date()  -->
-                <time dateitme="<?php echo get_the_date(DATE_W3C);?>"><?php echo get_the_date();?></time>
-                <!--サムネイル-->
-                <?php if(has_post_thumbnail()):?>
-                <p class="thumb thumb-archive">
-                    <!--個別ページへのリンク部分 the_permalink() -->
-                    <a href="<?php the_permalink();?>">
-                        <?php the_post_thumbnail('easiestwp-thumbnail');?>
-                    </a>
-                </p>
-                <?php endif;?>
-                <p>
-                    <!-- カテゴリー the_category() -->
-                    <!-- the_category()関数は 初期設定では区切り文字が指定されていないため、第一引数にカンマと半角スペースを区切り文字として指定する-->
-                    <?php the_category(', ');?>
-                </p>
-                <h3>
-                    <a href="<?php the_permalink();?>">
-                        <!--記事タイトル the_title() -->
-                        <?php the_title();?>
-                    </a>
-                </h3>
-                <!-- タグ名 the_tags() -->
-                <!-- 初期設定で複数のタグがある場合でもカンマと半角スペースで区切って表示される -->
-                <p>
-                    <?php the_tags();?>
-                </p>
-
-            </li>
-            <?php endwhile;?>
-        </ul>
-        <?php else:?>
-        <p>記事がありません。</p>
-        <?php endif;?>
-    </section>
-
-
-    <nav>
-        <!--
->>>>>>> wp_enqueue_scriptスクリプト読み込み
-        **
-        * get_the_posts_pagination関数を使用して
-        * ページネーションを作成する
-        *
-        -->
+    <!-- get_the_posts_pagination関数を使用して ページネーションを作成する -->
     <!--以下のダブルコート文字列内で変数展開させるために定義しておく-->
     <?PHP $uri = get_theme_file_uri(); ?>
     <!-- 矢印の画像で表示させるための画像img -->
